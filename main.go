d38cc017c49411c3a599f8e1c2fced38917778cc
Ryo MANZOKU <manzoku@mobcast.jp>
2015-08-26 08:14:06 +0000

    Elasticache pricing

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"io/ioutil"
 	"strconv"
 
-	"gopkg.in/yaml.v2"
-
 	"github.com/bitly/go-simplejson"
 	"github.com/k0kubun/pp"
 	"github.com/mob-manzoku/go-jsonp"
+	"gopkg.in/yaml.v2"
 )
 
 type instanceTypes map[string]*size
@@ -32,9 +31,17 @@ var awsPricingURLs = map[string]string{
 }
 
 func main() {
+	//region := "apac-tokyo"
 
-	region := "apac-tokyo"
-	service := "rds"
+	ec := GetElasticachePricing("ap-northeast-1")
+	pp.Print(ec)
+
+}
+
+func GetElasticachePricing(region string) instanceTypes {
+
+	service := "elasticache"
+	specpath := "spec/elasticache.yml"
 	ret := instanceTypes{}
 
 	str, _ := gojsonp.GetJSONFromURL(awsPricingURLs[service])
@@ -73,8 +80,7 @@ func main() {
 		}
 	}
 
-	path := "spec/rds.yml"
-	buf, err := ioutil.ReadFile(path)
+	buf, err := ioutil.ReadFile(specpath)
 	if err != nil {
 		panic(err)
 	}
@@ -87,7 +93,75 @@ func main() {
 		msize := v.(map[interface{}]interface{})["size"].(string)
 		mvCPU := v.(map[interface{}]interface{})["vCPU"].(int)
 		mmem := v.(map[interface{}]interface{})["memoryGiB"].(float64)
+		mnet := v.(map[interface{}]interface{})["network"].(string)
+
+		if _, ok := ret[msize]; ok {
+			ret[msize].vCPU = &mvCPU
+			ret[msize].memoryGiB = &mmem
+			ret[msize].network = &mnet
+		}
+
+	}
+
+	return ret
+}
+
+func GetRDSPricing(region string) instanceTypes {
+
+	service := "rds"
+	specpath := "spec/rds.yml"
+	ret := instanceTypes{}
+
+	str, _ := gojsonp.GetJSONFromURL(awsPricingURLs[service])
+
+	raw, err := simplejson.NewJson([]byte(str))
+	if err != nil {
+		panic(err)
+	}
+
+	regions := raw.Get("config").Get("regions")
+
+	for i := range regions.MustArray() {
+
+		if regions.GetIndex(i).Get("region").MustString() != region {
+			continue
+		}
+
+		types := regions.GetIndex(i).Get("types")
+
+		for j := range types.MustArray() {
+
+			tiers := types.GetIndex(j).Get("tiers")
+
+			for k := range tiers.MustArray() {
+
+				msize := tiers.GetIndex(k).Get("name").MustString()
+				mprice, _ := strconv.ParseFloat(tiers.GetIndex(k).Get("prices").Get("USD").MustString(), 64)
+				obj := &size{
+					size:  &msize,
+					price: &mprice,
+				}
+
+				ret[*obj.size] = obj
+			}
+
+		}
+	}
+
+	buf, err := ioutil.ReadFile(specpath)
+	if err != nil {
+		panic(err)
+	}
+
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(buf, &m)
+
+	for _, v := range m[service].([]interface{}) {
+
+		msize := v.(map[interface{}]interface{})["size"].(string)
+		mvCPU := v.(map[interface{}]interface{})["vCPU"].(int)
 		mpiop := v.(map[interface{}]interface{})["piopsOptimized"].(bool)
+		mmem := v.(map[interface{}]interface{})["memoryGiB"].(float64)
 		mnet := v.(map[interface{}]interface{})["network"].(string)
 
 		if _, ok := ret[msize]; ok {
@@ -99,8 +173,5 @@ func main() {
 
 	}
 
-	for k, v := range ret {
-		pp.Print(k, *v)
-	}
-
+	return ret
 }
