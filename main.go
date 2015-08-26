afc80ce2f990f839774ed94739cf774cd4ae9fa9
Ryo MANZOKU <manzoku@mobcast.jp>
2015-08-26 07:50:02 +0000

    RDS pricing

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"strconv"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/bitly/go-simplejson"
+	"github.com/k0kubun/pp"
 	"github.com/mob-manzoku/go-jsonp"
 )
 
-type instanceTypes map[string]size
+type instanceTypes map[string]*size
 
 type size struct {
-	size           string
-	vCPU           string
-	ECU            string
-	memoryGiB      string
-	storageGB      string
-	system         string
-	piopsOptimized string
-	price          string
+	size           *string
+	vCPU           *int
+	ECU            *float64
+	memoryGiB      *float64
+	storageGB      *float64
+	system         *string
+	piopsOptimized *bool
+	price          *float64
+	network        *string
 }
 
 var awsPricingURLs = map[string]string{
@@ -29,8 +34,10 @@ var awsPricingURLs = map[string]string{
 func main() {
 
 	region := "apac-tokyo"
+	service := "rds"
+	ret := instanceTypes{}
 
-	str, _ := gojsonp.GetJSONFromURL(awsPricingURLs["rds"])
+	str, _ := gojsonp.GetJSONFromURL(awsPricingURLs[service])
 
 	raw, err := simplejson.NewJson([]byte(str))
 	if err != nil {
@@ -52,31 +59,48 @@ func main() {
 			tiers := types.GetIndex(j).Get("tiers")
 
 			for k := range tiers.MustArray() {
-				fmt.Println(tiers.GetIndex(k).Get("name"))
-				fmt.Println(tiers.GetIndex(k).Get("prices").Get("USD"))
+
+				msize := tiers.GetIndex(k).Get("name").MustString()
+				mprice, _ := strconv.ParseFloat(tiers.GetIndex(k).Get("prices").Get("USD").MustString(), 64)
+				obj := &size{
+					size:  &msize,
+					price: &mprice,
+				}
+
+				ret[*obj.size] = obj
 			}
 
 		}
 	}
 
-	// path := "static/rds_spec.yml"
-	// buf, err := ioutil.ReadFile(path)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	path := "spec/rds.yml"
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
 
-	// m := make(map[interface{}]interface{})
-	// err = yaml.Unmarshal(buf, &m)
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(buf, &m)
 
-	// ret := instanceTypes{}
+	for _, v := range m[service].([]interface{}) {
 
-	// for _, v := range m["rds"].([]interface{}) {
+		msize := v.(map[interface{}]interface{})["size"].(string)
+		mvCPU := v.(map[interface{}]interface{})["vCPU"].(int)
+		mmem := v.(map[interface{}]interface{})["memoryGiB"].(float64)
+		mpiop := v.(map[interface{}]interface{})["piopsOptimized"].(bool)
+		mnet := v.(map[interface{}]interface{})["network"].(string)
 
-	// 	obj := size{
-	// 		size: v.(map[interface{}]interface{})["size"].(string),
-	// 	}
+		if _, ok := ret[msize]; ok {
+			ret[msize].vCPU = &mvCPU
+			ret[msize].memoryGiB = &mmem
+			ret[msize].piopsOptimized = &mpiop
+			ret[msize].network = &mnet
+		}
 
-	// 	ret[obj.size] = obj
-	// }
+	}
+
+	for k, v := range ret {
+		pp.Print(k, *v)
+	}
 
 }
